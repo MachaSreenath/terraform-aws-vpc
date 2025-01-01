8a271ee1303718254bb501ad18a62a03654248d4
MachaSreenath <sreenathshree9@gmail.com>
2025-01-01 18:34:52 +0530

    terraform-another-modules changes

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16" # users can override
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+  default = true
+}
+
+variable "common_tags" {
+  type = map
+  default = {} # it is optional
+}
+
+variable "vpc_tags" {
+  type = map
+  default = {} 
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
