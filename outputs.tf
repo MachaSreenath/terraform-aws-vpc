48e402512fe3086eee91cbfdb3c1a7ad585658ed
MachaSreenath <sreenathshree9@gmail.com>
2025-01-01 19:46:40 +0530

    terraform-another-modules changes

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # if we want to give any value to the end-users then we must write output block.
 
 output "azs" {
-  value = data.aws_availability_zones.azs
+  value = data.aws_availability_zones.azs.names
 }
